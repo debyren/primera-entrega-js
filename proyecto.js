72527f7ba8a7e3edb82a903e5b2026af8796de5d
Debora Benitez <deborabenitez@live.com.ar>
2022-10-31 17:13:49 -0300

    agrego dom y localstorage

--- proyecto.js
+++ proyecto.js
@@ -1,59 +1,142 @@
 
-function plazoFijo (numero, dias){
-    this.numero = numero; 
-    this.dias = dias;
-    console.log (dias)
-        switch (dias){
-            case 30:
-            return (numero * 75 /100 / 12 + numero);
-            break;
-            case 60:
-            return (numero * 75 / 100 /6 + numero);
-            break;
-            case 90:
-            return (numero * 75 / 100 /12 * 3 + numero);
-            break;
-            default:
-            return 0;
-            break;
-        }
+
+  let monto = document.getElementById('ingresoMonto')
+  let dias30 = document.getElementById('30dias') 
+  let dias60 = document.getElementById('60dias')
+  let dias90 = document.getElementById('90dias')
+  
+  let intereses = document.getElementById('interesGanado')
+  let hastaEl = document.getElementById('hasta')
+
+
+
+  //EVENTO CLICK 30 DIAS
+ 
+ dias30.addEventListener('click', () => {
+    const interes = plazoFijo(monto.value, dias30.value, intereses.value);
+    intereses.innerHTML = "$" + interes 
+   const meses = tiempo(tiempo.value)
+   hastaEl.innerHTML = meses
+
+   localStorage.setItem("interesGanado", interes)
+    localStorage.setItem("tiempoInvertido", meses )
+    console.log(interes)
+    console.log(meses)
+ })
+ 
+
+
+  //FUNCION PARA CALCULAR 30 DIAS A PARTIR DE LA FECHA ACTUAL
+  function tiempo(){	
+   
+    const diasDevolucion = 30;
+    let fechaDevolucion = new Date();
+   
+    fechaDevolucion.setDate(fechaDevolucion.getDate() + diasDevolucion);
+    let diaDevolucion = fechaDevolucion.getDate();
+    let mesDevolucion = fechaDevolucion.getMonth() + 1;
+    let anioDevolucion = fechaDevolucion.getFullYear();
+    let fechaFinal = diaDevolucion + "/" + mesDevolucion + "/" + anioDevolucion
+    return fechaFinal
+  }
+
+
+//FUNCION PARA CALCULAR INTERES A 30 DIAS
+    function plazoFijo(monto, dias30, intereses){
+      while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+    dias30 = 1
+    let resultadoFinal = 0
+    resultadoFinal = monto * (Math.pow(1+6.25/100, dias30)*6.25/100)/(Math.pow(1+6.25/100, dias30)-1);
+   
+    resultado.append("$" + resultadoFinal )
+    intereses = resultadoFinal - monto
+   
+    return intereses 
 }
 
-//cantidad de
-let plazoEnDias = [30, 60, 90]
-plazoEnDias.forEach ((n) =>{
-    console.log(n)
+
+//EVENTO CLICK 60 DIAS
+    dias60.addEventListener('click', () => {
+    const interes = plazoFijo2(monto.value, dias60.value, intereses.value);
+    intereses.innerHTML = "$" + interes 
+    const meses = tiempo2()
+    hastaEl.innerHTML = meses
+    
+    localStorage.setItem("interesGanado", interes)
+    localStorage.setItem("tiempoInvertido", meses )
+    console.log(interes)
+    console.log(meses)
 })
 
-let numero1 = parseInt(prompt("ingrese el importe"))
-while(numero1 < 1000){
-    alert("el importe tiene que ser minimo $1000")
-    numero1 = parseInt(prompt("ingrese el importe"))
-}
-let cantidadDias = parseInt(prompt("ingrese la cantidad de dias"))
-let resultado = plazoFijo(numero1, cantidadDias)
-
-
-while(cantidadDias < 1) {
-    alert("por favor ingrese 30, 60 o 90 dias")
-    cantidadDias = parseInt(prompt("ingrese la cantidad de dias"))
-    if(cantidadDias == 30) {
-        resultado = plazoFijo(numero1, cantidadDias)
-    }
-    if(cantidadDias == 60){
-        resultado = plazoFijo(numero1, cantidadDias)
-    }
-    if(cantidadDias == 90){
-        resultado = plazoFijo(numero1, cantidadDias)
-    }
-    else 
-    alert("ingrese un importe, minimo $1000")
-}
-
- alert("el resultado de su pazo fijo a " + cantidadDias + " dias es: $"  + resultado);
-
-
+//FUNCION PARA CALCULAR 60 DIAS A PARTIR DE LA FECHA ACTUAL
+    function tiempo2(){	
+   
+    const diasDevolucion = 60;
+    let fechaDevolucion = new Date();
+    fechaDevolucion.setDate(fechaDevolucion.getDate() + diasDevolucion);
+    let diaDevolucion = fechaDevolucion.getDate();
+    let mesDevolucion = fechaDevolucion.getMonth() + 1;
+    let anioDevolucion = fechaDevolucion.getFullYear();
+    let fechaFinal = diaDevolucion + "/" + mesDevolucion + "/" + anioDevolucion
   
+    return fechaFinal
+}
 
+ //FUNCION PARA CALCULAR INTERES A 60 DIAS
+  function plazoFijo2(monto, dias60, intereses,){
+    while(resultado.firstChild){
+      resultado.removeChild(resultado.firstChild);
+  }
+        dias60 = 1
+        let resultadoFinal = 0
+        resultadoFinal = monto * (Math.pow(1+12.5/100, dias60)*12.5/100)/(Math.pow(1+12.5/100, dias60)-1);
+        resultado.append("$" + resultadoFinal)
+        intereses = resultadoFinal - monto
+    return intereses
 
+  }
 
+  //EVENTO CLICK 90 DIAS
+  dias90.addEventListener('click', () => {
+   const interes = plazoFijo3(monto.value, dias90.value, intereses.value);
+   intereses.innerHTML = "$" + interes 
+   const meses = tiempo3()
+   hastaEl.innerHTML = meses 
+   localStorage.setItem("interesGanado", interes)
+    localStorage.setItem("tiempoInvertido", meses )
+    console.log(interes)
+    console.log(meses)
+  })
+
+  //FUNCION PARA CALCULAR 90 DIAS A PARTIR DE LA FECHA ACTUAL
+  function tiempo3(){	
+  const diasDevolucion = 90;
+  let fechaDevolucion = new Date();
+   
+  fechaDevolucion.setDate(fechaDevolucion.getDate() + diasDevolucion);
+  let diaDevolucion = fechaDevolucion.getDate();
+  let mesDevolucion = fechaDevolucion.getMonth() + 1;
+  let anioDevolucion = fechaDevolucion.getFullYear();
+  let fechaFinal = diaDevolucion + "/" + mesDevolucion + "/" + anioDevolucion
+    
+  return fechaFinal
+}
+
+ //FUNCION PARA CALCULAR INTERES A 90 DIAS
+function plazoFijo3(monto, dias90, intereses){
+  while(resultado.firstChild){
+    resultado.removeChild(resultado.firstChild);
+}
+    dias90 = 1
+   
+    let resultadoFinal = 0
+    resultadoFinal = monto * (Math.pow(1+18.75/100, dias90)*18.75/100)/(Math.pow(1+18.75/100, dias90)-1);
+    resultado.append( "$" + resultadoFinal)
+   
+    intereses = resultadoFinal - monto
+    return intereses
+    }
+    
+  
